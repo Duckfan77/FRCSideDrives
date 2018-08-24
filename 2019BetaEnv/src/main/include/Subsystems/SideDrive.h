1e394caf99082fc79f71c4e59031332f4c091988
Duckfan77 <cwenthin@yahoo.com>
2018-08-23 22:12:39 -0500

    First SideDrive setup/implementation
    The initial creation and setup of the abstract SideDrive class was done, defining the key functions that will be necessary.

---
+++ 2019BetaEnv/src/main/include/Subsystems/SideDrive.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <math.h>
+#include <AHRS.h>
+#include <WPILib.h>
+
+class SideDrive : frc::Subsystem
+{
+    private:
+        SideDrive(std::string name);
+    protected:
+        AHRS* m_navX;
+        DifferentialDrive* m_drive;
+        bool m_bNavXPresent;
+
+        virtual void CartesianToPolar(float x, float y, float rotate, bool squaredInputs = false);
+        virtual void PolarToCartesian(float m, float theta, float rotate, bool squaredInputs = false);
+    public:
+        ~SideDrive();
+
+        /**
+         * 
+         * @param x The rate of travel in the x direction
+         * @param y The rate of travel in the y direction
+         * @param rotate The rate of rotation, unrelated to x and y
+         * @param squaredInputs Whether to square the input values, usually true for manual driving
+         */
+        virtual void DriveCartesian(float x, float y, float rotate, bool squaredInputs = false)=0;
+
+        /**
+         * 
+         * @param m The magnitude of travel
+         * @param theta The angle at which to travel
+         * @param rotate The rate of rotation, unrleated to m and theta
+         * @param squaredInputs Whether to square the input values, usually true for manual driving
+         */
+        virtual void DrivePolar(float m, float theta, float rotate, bool squaredInputs=false)=0;
+
+
+};
