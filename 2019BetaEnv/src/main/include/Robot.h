751f4337fc4e4890cc5efbdb14ca42a41c79abf4
Duckfan77 <cwenthin@yahoo.com>
2018-09-01 20:43:32 -0500

    Finish removing unneccesary files

--- 2019BetaEnv/src/main/include/Robot.h
+++ 2019BetaEnv/src/main/include/Robot.h
@@ -11,14 +11,11 @@
 #include <SmartDashboard/SendableChooser.h>
 #include <TimedRobot.h>
 
-#include "Commands/ExampleCommand.h"
 #include "Commands/MyAutoCommand.h"
 #include "OI.h"
-#include "Subsystems/ExampleSubsystem.h"
 
 class Robot : public frc::TimedRobot {
  public:
-  static ExampleSubsystem m_subsystem;
   static OI* m_oi;
 
   void RobotInit() override;
@@ -35,7 +32,5 @@ class Robot : public frc::TimedRobot {
   // Have it null by default so that if testing teleop it
   // doesn't have undefined behavior and potentially crash.
   frc::Command* m_autonomousCommand = nullptr;
-  ExampleCommand m_defaultAuto;
-  MyAutoCommand m_myAuto;
   frc::SendableChooser<frc::Command*> m_chooser;
 };
